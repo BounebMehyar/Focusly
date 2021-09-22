aa94b386cc3c9827ed81bbbd87fb0b3e12d6b302
Rayan Bouneb <rayenbouneb01@gmail.com>
2021-09-22 09:57:35 +0100

    version beta

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,6 +1,9 @@
 import {useState} from 'react';
 import '../Styles/Timer.css';
+import ding from '../Assets/ding.mp3';
 let interval;
+let o=true;
+let duration2=5*60;
 var duration= 25*60;
 var timer = duration, minutes, seconds;
 
@@ -22,7 +25,20 @@ function Timer(){
             setStr("Stop");
     
             if (--timer < 0) {
-                timer = duration;
+                timer = o ? duration2:duration;
+                o=!o;
+                var audio = document.getElementById("ding");
+                audio.play();
+                clearInterval(interval);
+        setStr("Play");
+        minutes = parseInt(timer / 60, 10);
+            seconds = parseInt(timer % 60, 10);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            setTimer(minutes + ":" + seconds);
+
             }
         }, 1000);}
     else{clearInterval(interval);
@@ -30,13 +46,15 @@ function Timer(){
 
     function stop(){
         if(interval){clearInterval(interval);
-        setTimer('25:00');
+        if(o){setTimer('25:00');timer=25*60;}
+        else{setTimer('05:00');timer=5*60;}
     setStr("Play");}
     }
     return(<div className="timer-div">
         <h1 className="time">{timerr}</h1>
         <button onClick={start} className="start-button">{str}</button>
         <button  className="Reset-button" onClick={stop}>Reset</button>
+        <audio id="ding" src={ding}></audio>
         </div>
     );
 }
