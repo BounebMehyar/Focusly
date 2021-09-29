042b5258fd28788d8ba96a5d89a9f2f22ce93f1b
Rayan Bouneb <rayenbouneb01@gmail.com>
2021-09-29 19:32:22 +0100

    added local Storage to the tasks

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,18 +1,40 @@
 import { useState } from "react";
 import '../Styles/Tasks.css';
 function Tasks(){
-    const [tasklist,settasks]=useState([]);
-    function deleteEle(x){const f= tasklist.filter(
-        (plant) => plant !== x)
-        settasks(f);
+    const [tasklist,settasks]=useState(localStorage.getItem('list') ==='' ? []:localStorage.getItem('list').split(','));
+
+    var addToLocalStorageArray = function (name, value) {
+        var existing = localStorage.getItem(name);
+        existing = existing ? existing.split(',') : [];
+        existing.push(value);
+        localStorage.setItem(name, existing.toString());
+    };
+
+    function addEle(){
+        addToLocalStorageArray("list",document.getElementById("input").value);
+        settasks(localStorage.getItem("list").split(','));
+        document.getElementById("input").value="";
+    }
+
+    var removefromLocalStorageArray = function (name, value) {
+        var existing = localStorage.getItem(name).split(',');
+        const f= existing.filter(
+            (plant) => plant !== value);
+        localStorage.setItem(name, f.toString());
+    };
+
+
+    function deleteEle(x){removefromLocalStorageArray('list',x);
+        if(localStorage.getItem("list")===""){settasks([]);}
+        else{settasks(localStorage.getItem("list").split(','));}
     }
     return(
         <div className="tasks-div">
            <h2>Tasks:</h2>
            <ul>
-           {tasklist.map(ele=>(<li>{ele.data}<button className="tasks-buttons" onClick={()=>deleteEle(ele)}> <i class="far fa-trash-alt"></i></button></li>))}
+           {tasklist.map(ele=>(<li>{ele}<button className="tasks-buttons" onClick={()=>deleteEle(ele)}> <i class="far fa-trash-alt"></i></button></li>))}
            </ul>
-           <input id="input" maxLength="30" autoComplete="off"></input><button className="tasks-buttons" onClick={()=>{settasks([...tasklist,{data:document.getElementById("input").value,check:false}]);document.getElementById("input").value=""}}><i class="fas fa-plus"></i></button>
+           <input id="input" maxLength="30" autoComplete="off"></input><button className="tasks-buttons" onClick={addEle}><i class="fas fa-plus"></i></button>
         </div>
     );
 }
